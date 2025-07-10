dc9655e66bc6dab18ca87a513cf524b0cf617500
Björn Sterling <bjorn.sterling@gmail.com>
2025-07-10 16:03:02 +0200

    10 divs create in one column

---
+++ js-file.js
@@ -0,0 +1,19 @@
+// select the container element in the page
+let container = document.querySelector("#container");
+
+// Create a new dic element
+// const newDiv = document.createElement("div");
+
+// add a class to the div
+// newDiv.classList.add("divGrid");
+
+// Append the new div to the container
+// container.appendChild(newDiv);
+
+// create 10 divs inside the container
+for (let i = 1; i <= 10; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.textContent = "div";
+    newDiv.classList.add("divGrid");
+    container.appendChild(newDiv);
+}
